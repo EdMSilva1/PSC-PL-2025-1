3f5b8dc374a7fd59f5dc9b7b0f2e81bb482ef1fd
EdMSilva1 <825160638@ulife.com.br>
2025-03-14 10:58:25 -0300

    Q

---
+++ entrada.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class entrada{
+    public static void main(String[] args){
+Scanner scanner = new Scanner(System.in);
+
+System.out.println(" Digite o seu nome ");
+String nome = scanner.nextLine();
+
+System.err.println(" digite a sua idade ");
+int idade = scanner.nextInt();
+
+System.err.println(" Olá " + nome + " Você tem " + idade + " anos, seja bem vindo(a)");
+
+    }
+}
